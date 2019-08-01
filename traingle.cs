b2d2da1e85757102574a36548e7a70f034ac1168
Vijaya04-alt <53554854+Vijaya04-alt@users.noreply.github.com>
2019-08-01 12:22:46 +0530

    Add files via upload
    Hi Khaja,
    
    I have uploaded the file.

---
+++ traingle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge
+{
+    class traingle
+    {
+
+        int a, b, c;
+        public static void Main(String[] args)
+        {
+            int a = 7, b = 10, c = 5;
+
+            if (checkValidity(a, b, c))
+            {
+                Console.WriteLine("The given integer values forms the triangle");
+            }
+            else
+                Console.WriteLine("The given integer values doesn't  forms the triangle");
+
+        }
+
+
+        static Boolean checkValidity(int a, int b, int c)
+        {
+            // check condition 
+            if (a + b <= c || a + c <= b || b + c <= a)
+                return false;
+            else
+                return true;
+        }
+    }
+}
