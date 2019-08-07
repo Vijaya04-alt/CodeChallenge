d29d15e05269bdb8865c1dfe1ebb52bdbd6c4018
Vijaya04-alt <53554854+Vijaya04-alt@users.noreply.github.com>
2019-08-07 18:50:45 +0530

    Add files via upload
    Hi Khaja,
    I have committed the code.
    Regards,
    Sravan

---
+++ codechallenge3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QAChallenge
+{
+   
+    
+        class codechallenge3
+        {
+            static Random rnd = new Random();
+            static void Main(string[] args)
+            {
+                var responses = new List<string>() { "Thi2s ", "is4 ", "a2 ", "book3 " };
+                var shuffle = new List<string>();
+                for (int ix = responses.Count; ix > 0; --ix)
+                {
+                    int index = rnd.Next(0, ix);
+                    shuffle.Add(responses[index]);
+                    responses.RemoveAt(index);
+                }
+                foreach (string word in shuffle)
+                    Console.Write(word);
+                Console.ReadLine();
+            }
+        }
+
+    }
+
